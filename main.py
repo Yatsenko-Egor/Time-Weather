5f7ad6b245aea7fb90c0a6e736cabdd3042ec5c9
Yatsenko-Egor <74555653+Yatsenko-Egor@users.noreply.github.com>
2021-04-27 21:31:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     rus = []
     ext = []
@@ -72,5 +73,4 @@ def error_not_found(error):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=8000, host='127.0.0.1')
