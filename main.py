26f0c6ac26dc34fa112a45eefd6289f722563f31
Yatsenko-Egor <74555653+Yatsenko-Egor@users.noreply.github.com>
2021-04-27 21:34:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -73,4 +73,5 @@ def error_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(port=8000, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
