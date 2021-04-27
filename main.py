ca0d3187fa5506c6cb1ac3acf6a16a5655186a91
Yatsenko-Egor <74555653+Yatsenko-Egor@users.noreply.github.com>
2021-04-27 16:21:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -72,6 +72,6 @@ def error_not_found(error):
     return render_template("error_file.html", **params)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
