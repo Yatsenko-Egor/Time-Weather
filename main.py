63442db5c61a1bd142d89a5c1addcb75cb0efd7b
Yatsenko-Egor <74555653+Yatsenko-Egor@users.noreply.github.com>
2021-04-28 21:34:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -75,6 +75,6 @@ def error_not_found(error):
 
 
 if __name__ == '__main__':
-    run_telegram_bot()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+    run_telegram_bot()
